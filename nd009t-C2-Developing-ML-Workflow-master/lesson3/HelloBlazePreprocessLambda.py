90abc0ef04d3620f454e1170f1430aef84ffae11
detc0n <m.conan@protonmail.ch>
2022-05-01 13:24:11 +0200

    add lesson3 - exercise lambda code and files

--- nd009t-C2-Developing-ML-Workflow-master/lesson3/HelloBlazePreprocessLambda.py
+++ nd009t-C2-Developing-ML-Workflow-master/lesson3/HelloBlazePreprocessLambda.py
@@ -1,21 +1,23 @@
-
 import json
+import logging
 import zipfile
 import os
 import boto3
 from botocore.exceptions import ClientError
 
-BUCKET_NAME = 'FILL_THIS_IN'
-PREFIX = 'FILL_THIS_IN'
+BUCKET_NAME = 'mk-aws-ml-engineer-nanodegree-2022'
+PREFIX = 'exercises/lesson3/HelloBlazePreprocessLambda-output/'
 
-# Function below unzips the archive to the local directory. 
+
+# Function below unzips the archive to the local directory.
 
 def unzip_data(input_data_path):
     with zipfile.ZipFile(input_data_path, 'r') as input_data_zip:
         input_data_zip.extractall('/tmp/')
         return '/tmp/' + input_data_zip.namelist()[0]
 
-# Input data is a file with a single JSON object per line with the following format: 
+
+# Input data is a file with a single JSON object per line with the following format:
 # {
 #  "reviewerID": <string>,
 #  "asin": <string>,
@@ -38,7 +40,7 @@ def label_data(input_data):
     labeled_data = []
     HELPFUL_LABEL = "__label__1"
     UNHELPFUL_LABEL = "__label__2"
-     
+
     for l in open(input_data, 'r'):
         l_object = json.loads(l)
         helpful_votes = float(l_object['helpful'][0])
@@ -49,7 +51,7 @@ def label_data(input_data):
                 labeled_data.append(" ".join([HELPFUL_LABEL, reviewText]))
             elif helpful_votes / total_votes < .5:
                 labeled_data.append(" ".join([UNHELPFUL_LABEL, reviewText]))
-          
+
     return labeled_data
 
 
@@ -58,12 +60,13 @@ def label_data(input_data):
 def split_sentences(labeled_data):
     new_split_sentences = []
     for d in labeled_data:
-        label = d.split()[0]        
-        sentences = " ".join(d.split()[1:]).split(".") # Initially split to separate label, then separate sentences
+        label = d.split()[0]
+        sentences = " ".join(d.split()[1:]).split(".")  # Initially split to separate label, then separate sentences
         for s in sentences:
-            if s: # Make sure sentences isn't empty. Common w/ "..."
+            if s:  # Make sure sentences isn't empty. Common w/ "..."
                 new_split_sentences.append(" ".join([label, s]))
     return new_split_sentences
+
 
 def upload_data(file_name):
     object_name = os.path.join(PREFIX, os.path.basename(file_name))
@@ -74,9 +77,10 @@ def upload_data(file_name):
         logging.error(e)
         return False
 
+
 def write_data(data, b_name, proportion):
     train_path = '/tmp/' + b_name + '_train'
-    test_path = '/tmp/' +  b_name + '_test'
+    test_path = '/tmp/' + b_name + '_test'
     border_index = int(proportion * len(data))
     train_f = open(train_path, 'w')
     test_f = open(test_path, 'w')
@@ -92,6 +96,7 @@ def write_data(data, b_name, proportion):
     upload_data(train_path)
     upload_data(test_path)
 
+
 def download_data(s3_input_uri):
     s3 = boto3.client('s3')
     input_bucket = s3_input_uri.split('/')[0]
@@ -99,11 +104,11 @@ def download_data(s3_input_uri):
     file_name = '/tmp/' + os.path.basename(input_object)
     s3.download_file(input_bucket, input_object, file_name)
     return file_name
-        
+
+
 def preprocess(s3_input_uri):
     f_name = download_data(s3_input_uri)
     unzipped_path = unzip_data(f_name)
     labeled_data = label_data(unzipped_path)
     new_split_sentence_data = split_sentences(labeled_data)
     write_data(new_split_sentence_data, os.path.basename(s3_input_uri), .9)
-
