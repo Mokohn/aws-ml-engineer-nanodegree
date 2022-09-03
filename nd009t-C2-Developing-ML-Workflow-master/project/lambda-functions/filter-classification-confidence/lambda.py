de377b171968c8513bc8f5a885ada7eef43e1362
kohnen <m.conan@protonmail.ch>
2022-09-03 15:11:54 +0000

    finish project

--- nd009t-C2-Developing-ML-Workflow-master/project/lambda-functions/filter-classification-confidence/lambda.py
+++ nd009t-C2-Developing-ML-Workflow-master/project/lambda-functions/filter-classification-confidence/lambda.py
@@ -1,5 +1,3 @@
-import json
-
 THRESHOLD = .90
 
 
@@ -9,17 +7,18 @@ def lambda_handler(event, context):
     inferences = event['inferences']
 
     # Check if any values in our inferences are above THRESHOLD
-    for inference in inferences:
-        meets_threshold = inference > THRESHOLD
+    meets_threshold = [inference > THRESHOLD for inference in inferences]
+    print(f'Meets treshold: {meets_threshold}')
 
-        # If our threshold is met, pass our data back out of the
-        # Step Function, else, end the Step Function with an error
-        if meets_threshold:
-            pass
-        else:
-            raise Exception(f"THRESHOLD_CONFIDENCE_NOT_MET, INFERENCES {inferences}")
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps(event)
+    if True in meets_threshold:
+    # If our threshold is met, pass our data back out of the
+    # Step Function, else, end the Step Function with an error
+    #if meets_threshold:
+        pass
+    else:
+        raise Exception(f"THRESHOLD_CONFIDENCE_NOT_MET, INFERENCES {inferences}")
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
         }
