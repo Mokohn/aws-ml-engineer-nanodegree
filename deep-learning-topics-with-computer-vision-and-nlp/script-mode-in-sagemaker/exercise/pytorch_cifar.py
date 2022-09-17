307ce2dc95ccbdd7dc1e7377d56d1da34324d354
detc0n <m.conan@protonmail.ch>
2022-09-17 12:18:40 +0200

    update pytorch_cifar.py for exercise script mode in sagemaker

--- deep-learning-topics-with-computer-vision-and-nlp/script-mode-in-sagemaker/exercise/pytorch_cifar.py
+++ deep-learning-topics-with-computer-vision-and-nlp/script-mode-in-sagemaker/exercise/pytorch_cifar.py
@@ -5,9 +5,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 
+
 class Net(nn.Module):
     def __init__(self):
-        #TODO: Complete this function
+        # TODO: Complete this function
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
@@ -15,9 +16,9 @@ class Net(nn.Module):
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
-        
+
     def forward(self, x):
-        #TODO: Complete the forward function
+        # TODO: Complete the forward function
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
@@ -31,6 +32,7 @@ class Net(nn.Module):
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
+
 
 def train(model, train_loader, optimizer, epoch):
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -98,18 +100,19 @@ def main():
     parser.add_argument(
         "--learning-rate", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)"
     )
-    
+
     args = parser.parse_args()
 
     train_kwargs = {"batch_size": args.batch_size}
     test_kwargs = {"batch_size": args.test_batch_size}
 
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])  # TODO: Create your transform
-    
+    transform = transforms.Compose([transforms.ToTensor()])  # TODO: Create your transform
+
     # TODO: Add the CIFAR10 dataset and create your data loaders
-    dataset = datasets.CIFAR10("../data", train=True, download=True, transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset, **train_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset, **test_kwargs)
+    dataset1 = datasets.CIFAR10("../data", train=True, download=True, transform=transform)
+    dataset2 = datasets.CIFAR10("../data", train=False, download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net()
 
@@ -118,6 +121,8 @@ def main():
     for epoch in range(1, args.epochs + 1):
         train(model, train_loader, optimizer, epoch)
         test(model, test_loader)
+
+    torch.save(model.state_dict(), "mnist_cnn.pt")
 
 
 if __name__ == "__main__":
